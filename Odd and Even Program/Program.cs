2c68e7eb8b712abbe21b00579967771625abb4ae
Utkarsh Singh <utkarshsinghmca@gmail.com>
2018-10-03 13:13:42 +0530

    Day 1

--- Odd and Even Program/Program.cs
+++ Odd and Even Program/Program.cs
@@ -1,5 +1,5 @@
 using System;
-namespace First
+namespace ConsoleApp1
 {
     class Program
     {
@@ -8,11 +8,11 @@ namespace First
             int num;
             Console.Write("Ek number dalo check karne k liye kya wo even number hai ya phir odd number hai bhai");
             num = int.Parse(Console.ReadLine());
-            if(num % 2 == 0)
+            if (num % 2 == 0)
             {
                 Console.Write("Ye wala number even hai bhai");
                 Console.Read();
-              
+
             }
             else
             {
