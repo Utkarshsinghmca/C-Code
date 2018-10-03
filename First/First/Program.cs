b58431e7e867b5163ade9a8c1940acd89fd39b37
Utkarsh Singh <utkarshsinghmca@gmail.com>
2018-10-03 12:53:02 +0530

    DAY 1

---
+++ First/First/Program.cs
@@ -0,0 +1,24 @@
+using System;
+namespace First
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            Console.Write("Ek number dalo check karne k liye kya wo even number hai ya phir odd number hai bhai");
+            num = int.Parse(Console.ReadLine());
+            if(num % 2 == 0)
+            {
+                Console.Write("Ye wala number even hai bhai");
+                Console.Read();
+              
+            }
+            else
+            {
+                Console.Write("Ye wala number odd hai bhai");
+                Console.Read();
+            }
+        }
+    }
+}
